436a653a4cec1e69784044a3dfc98623fd460815
Jhonatan Ascari <desenvolvimento01@ztech.inf.br>
2019-06-12 13:56:15 -0300

    Create routes.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
 import React from 'react';
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-=======
->>>>>>> 015eac96f1e639b77efa7370ee01bc7b3fc9c0c7
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
       <h1>Hello world!</h1>
->>>>>>> 015eac96f1e639b77efa7370ee01bc7b3fc9c0c7
     </div>
   );
 }
